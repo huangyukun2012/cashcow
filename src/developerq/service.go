de358d4743043728babdafe34f35e217281d7b83
Gengsheng Liu <gsliu@freewheel.tv>
2017-04-14 13:36:48 +0800

    update db connection

--- src/developerq/service.go
+++ src/developerq/service.go
@@ -106,8 +106,8 @@ func Init() {
 	if err := db.Ping(); err != nil {
 		panic("Error Connection database...")
 	}
-	db.SetMaxOpenConns(5000)
-	db.SetMaxIdleConns(1000)
+	db.SetMaxOpenConns(200)
+	db.SetMaxIdleConns(20)
 
 	u.LISTMAX = 300
 	u.PAGEMAX = 20
