233533218d4d9794ebd5f2fee3ce841414856a74
gengsheng LIu <gsliu.tju@gmail.com>
2017-04-19 01:05:08 +0000

    update monitor

--- monitor.sh
+++ monitor.sh
@@ -1,14 +1,23 @@
 #! /bin/bash
 
-case "$(pidof server | wc -w)" in
+checkserver() {
+	res=`/usr/sbin/pidof server|wc -l`
+	case "$res" in
+	0)  echo "Restarting server:     $(date)" >> /tmp/monitor.log
+    	t=`date`
+    	/cashcow/release/server > "/tmp/std-$t.log" 2>&1  &
+    	;;
+	1)  # all ok
+    	;;
+	*)  echo "Removed double server: $(date)" >> /monitor.logv
+    	kill $(pidof server | awk '{print $1}')
+    	;;
+	esac
+}
 
-0)  echo "Restarting server:     $(date)" >> /tmp/monitor.log
-    t=`date`
-    /cashcow/release/server > "/tmp/std-$t.log" 2>&1  &
-    ;;
-1)  # all ok
-    ;;
-*)  echo "Removed double server: $(date)" >> /monitor.logv
-    kill $(pidof server | awk '{print $1}')
-    ;;
-esac
+while true
+do
+  # loop infinitely
+	checkserver
+	sleep 5
+done
